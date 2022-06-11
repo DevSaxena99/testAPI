72100f0c625659d8945ddc4773546749cacd9c0b
Dev <saxenadev04@gmail.com>
2022-06-11 15:04:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
+import ReactDOM from 'react-dom/client';
 import './App.css';
 
+
+
 function App() {
+
+  const [count,setCount]=useState(0);
+
+  useEffect(() => {
+
+      var currTime = new Date();
+      // var currentOffset = currTime.getTimezoneOffset();
+      //
+      // var ISTOffset = 330;
+      //
+      // var ISTTime = new Date(currTime.getTime() + (ISTOffset + currentOffset)*60000);
+
+      // var hoursIST = ISTTime.getHours();
+      // var minutesIST = ISTTime.getMinutes();
+      const currentTime = currTime.getHours();
+
+      const execTime = 16;
+
+      let timeLeft;
+      if(currentTime < 16) {
+
+          timeLeft = execTime*60*60*1000 - currTime*60*60*1000;
+      } else {
+
+          timeLeft = execTime*60*60*1000 + 86400000 - currentTime*60*60*1000;
+      }
+
+      setTimeout(function() {
+        setInterval(function() {
+
+          setCount(count+1);
+
+      }, 86400000);
+    }, timeLeft);
+
+  },{});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Show Count</h2>
+      <h5>{count}</h5>
     </div>
   );
 }
